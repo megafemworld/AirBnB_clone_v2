5afeee28b1bc452e0bfbe644c31a21571fd5b36d
Femi Mehalayese <megafemgroup@gmail.com>
2024-07-12 00:18:12 +0100

    deploy 6

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -32,21 +32,24 @@ def do_deploy(archive_path):
         Distribute archive.
     """
     if os.path.exists(archive_path):
-        archived_file = archive_path[9:]
-        newest_version = "/data/web_static/releases/" + archived_file[:-4]
-        archived_file = "/tmp/" + archived_file
-        put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(newest_version))
-        run("sudo tar -xzf {} -C {}/".format(archived_file,
+        try:
+            archived_file = archive_path[9:]
+            newest_version = "/data/web_static/releases/" + archived_file[:-4]
+            archived_file = "/tmp/" + archived_file
+            put(archive_path, "/tmp/")
+            run("sudo mkdir -p {}".format(newest_version))
+            run("sudo tar -xzf {} -C {}/".format(archived_file,
                                              newest_version))
-        run("sudo rm {}".format(archived_file))
-        run("sudo mv {}/web_static/* {}".format(newest_version,
+            run("sudo rm {}".format(archived_file))
+            run("sudo mv {}/web_static/* {}".format(newest_version,
                                                 newest_version))
-        run("sudo rm -rf {}/web_static".format(newest_version))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(newest_version))
+            run("sudo rm -rf {}/web_static".format(newest_version))
+            run("sudo rm -rf /data/web_static/current")
+            run("sudo ln -s {} /data/web_static/current".format(newest_version))
 
         print("New version deployed!")
         return True
+    except:
+        return False`
 
     return False
