dcb4a305ccb47e2876b5850c814070bbd70e0269
Femi Mehalayese <megafemgroup@gmail.com>
2024-07-28 18:51:03 +0100

    hello Hbnb

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,17 +8,16 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello():
     """Say something on start
     listening: 0.0.0.0
     port: 5000
     """
 
-    return "Hello HBNB"
+    return "Hello HBNB!"
 
 
 if __name__ == '__main__':
 
-    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
