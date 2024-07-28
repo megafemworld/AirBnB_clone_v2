ea518f9c4becb5d426b74247f68a65aadc592336
Femi Mehalayese <megafemgroup@gmail.com>
2024-07-28 18:56:45 +0100

    hbnb

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Flask app starting
+flask script to start a web app
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Say something on start
+    listening: 0.0.0.0
+    port: 5000
+    """
+
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """HBNB output"""
+
+    return "HBNB"
+
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', port=5000)
