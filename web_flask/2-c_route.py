c41452d7a3a86e0c7436663113fd68fe5d90e1b9
Femi Mehalayese <megafemgroup@gmail.com>
2024-07-28 19:19:17 +0100

    text

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Flask app starting
+flask script to start a web app
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Say something on start
+    listening: 0.0.0.0
+    port: 5000
+    """
+
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """HBNB output"""
+
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c(text):
+    """ display “C ”, followed by the value
+    of the text variable"""
+
+    text = text.replace("_", " ")
+    return f'C {text}' 
+
+if __name__ == '__main__':
+
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
