23c978792e5aa99f88d6526436a905b84d33185c
Femi Mehalayese <megafemclick@gmail.com>
2024-09-12 15:32:15 +0100

    test db

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- Setup Mysql 
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFY BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* to 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_shema`.* to'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
