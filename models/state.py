5a6b7079b713bf2e9d926c8daac4045c16df92d9
Femi Mehalayese <megafemclick@gmail.com>
2024-09-27 16:59:51 +0100

    state error

--- models/state.py
+++ models/state.py
@@ -5,9 +5,4 @@ from models.base_model import BaseModel
 
 class State(BaseModel):
     """ State class """
-    if models.storage_t = "db":
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
-    else:
-        name = ""
+    name = ""
