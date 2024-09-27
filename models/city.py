b25454f37562457272f1509133cd24dfb6a250a4
Femi Mehalayese <megafemclick@gmail.com>
2024-09-27 17:00:48 +0100

    all db error

--- models/city.py
+++ models/city.py
@@ -5,10 +5,6 @@ from models.base_model import BaseModel
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    if models.storage_t == "db":
-        __tablename__ = 'cities'
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), nullable=False, ForeignKey('state.id'))
-    else:
-        state_id = ""
-        name = ""
+
+    state_id = ""
+    name = ""
