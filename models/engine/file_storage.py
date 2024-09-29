69c8c45485686c3431c707b4a5d4add140e8bbfa
Femi Mehalayese <megafemclick@gmail.com>
2024-09-29 23:32:39 +0100

    fixed

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,6 +10,12 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls is not None:
+            new_dict = {}
+            for key, value in self.__objects.items():
+                if cls == value.__class__ or cls == value.__class__.__name__:
+                    new_dict[key] = value
+            return new_dict
         return FileStorage.__objects
 
     def new(self, obj):
